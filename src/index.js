b35cfdaa788cc54c8230f3cf9a419eafce36d1f9
ngocnbhe141240 <ngocnbhe141240@fpt.edu.vn>
2023-11-25 14:53:20 +0700

    Create Controller

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+const route = require('./routes')
 
-app.use(morgan('combined'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}
+));
+app.use(express.json());
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', engine({
@@ -17,14 +24,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-    res.render('news')
-})
+//Routes init
+route(app);
 
 // 127.0.0.1
 app.listen(port, () => {
